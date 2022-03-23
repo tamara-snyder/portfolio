80d281d176156d088e4f8d8b8775d8ec3024423e
tamara-snyder <tamara.snyder@yale.edu>
2022-03-23 10:50:05 -0400

    Remove commented code and change overview h1 to purple since the rainbow was too busy

--- src/Overview.svelte
+++ src/Overview.svelte
@@ -30,12 +30,11 @@
 </div>
 
 <style>
-    .background {
+  .background {
     padding: 5vh 0;
     margin: 0 auto;
     width: 100%;
     height: 90vh;
-    /* background-image: linear-gradient( 109.6deg,  rgba(247,253,166,1) 11.2%, rgba(128,255,221,1) 57.8%, rgba(255,128,249,1) 85.9% ); */
     color: black;
   }
 
@@ -66,74 +65,19 @@
     margin: 5vh auto;
     letter-spacing: .1em;
     max-width: 47vw;
-    /* background-color: #6c5ce7; */
-  
-  /* Create the gradient. */
-   /* background: linear-gradient(
-        45deg,
-        #CA4246 16.666%, 
-        #E16541 16.666%, 
-        #E16541 33.333%, 
-        #F18F43 33.333%, 
-        #F18F43 50%, 
-        #8B9862 50%, 
-        #8B9862 66.666%, 
-        #476098 66.666%, 
-        #476098 83.333%, 
-        #A7489B 83.333%); */
-
-        /* background: linear-gradient(
-        45deg,
-        #ff7979 20%, 
-        #feca57 20%, 
-        #feca57 33.333%, 
-        #55efc4 33.333%, 
-        #55efc4 50%, 
-        #74b9ff 50%, 
-        #74b9ff 66.666%, 
-        #6c5ce7 66.666%, 
-        #6c5ce7 83.333%, 
-        #574b90 83.333%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent; */
-
-
-    background: #CA4246;
-  background-color: #CA4246;
-  background: conic-gradient(
-    #ff7979 20%, 
-        #feca57 20%, 
-        #feca57 33.333%, 
-        #55efc4 33.333%, 
-        #55efc4 50%, 
-        #74b9ff 50%, 
-        #74b9ff 66.666%, 
-        #6c5ce7 66.666%, 
-        #6c5ce7 83.333%, 
-        #574b90 83.333%);
-  
-  /* Set thee background size and repeat properties. */
-  background-size: 100%;
-  background-repeat: repeat;
-  
-  /* Use the text as a mask for the background. */
-  /* This will show the gradient as a text color rather than element bg. */
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent; 
+    background-color: #5f27cd;
   }
 
   h2 {
     font-size: 6vmin;
     text-align: center;
     margin: 5vh auto;
-
     overflow: hidden; /* Ensures the content is not revealed until the animation */
-  border-right: .1em solid orange; /* The typwriter cursor */
-  white-space: nowrap; /* Keeps the content on a single line */
-  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
-  letter-spacing: .1em; /* Adjust as needed */
-  /* animation: 
+    border-right: .1em solid #feca57; /* The typwriter cursor */
+    white-space: nowrap; /* Keeps the content on a single line */
+    margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+    letter-spacing: .1em; /* Adjust as needed */
+    /* animation: 
     typing 3s steps(40, end),
     blink-caret .75s step-end infinite; */
     animation:
@@ -145,24 +89,16 @@
     display: inline-block;
   }
 
-  /* h2::after {
-    content: "";
-    position: absolute;
-    animation: caret infinite;
-    animation-duration: 1s;
-    animation-timing-function: steps(1, end);
-} */
-
   @keyframes typing {
-  from { width: 0 }
-  to { width: 100% }
-}
+    from { width: 0 }
+    to { width: 100% }
+  }
 
-/* The typewriter cursor effect */
-@keyframes blink-caret {
-  from, to { border-color: transparent }
-  50% { border-color: #ffbe76; }
-}
+  /* The typewriter cursor effect */
+  @keyframes blink-caret {
+    from, to { border-color: transparent }
+    50% { border-color: #feca57; }
+  }
 
   .media-icon {
     width: 64px;
